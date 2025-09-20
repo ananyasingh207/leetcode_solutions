3ccff581496a8172428c9f9c349a801ca18d742c
Ananya Singh <ananyasingh10518@gmail.com>
2025-09-21 01:22:33 +0530

    Search in a rotated sorted array

---
+++ arrays/searchInrotatedSortedArray.cpp
@@ -0,0 +1,44 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution {
+public:
+    int search(vector<int>& nums, int target) {
+        int low = 0;
+        int high = nums.size() - 1;
+        
+        while (low <= high) {
+            int mid = low + (high - low) / 2;
+
+            if (nums[mid] == target) {
+                return mid;
+            }
+
+            // Left half is sorted
+            if (nums[low] <= nums[mid]) {
+                if (nums[low] <= target && target < nums[mid]) {
+                    high = mid - 1;
+                } else {
+                    low = mid + 1;
+                }
+            } 
+            // Right half is sorted
+            else {
+                if (nums[mid] < target && target <= nums[high]) {
+                    low = mid + 1;
+                } else {
+                    high = mid - 1;
+                }
+            }
+        }
+        return -1;
+    }
+};
+
+int main() {
+    Solution sol;
+    vector<int> nums = {4,5,6,7,0,1,2};
+    int target = 0;
+    cout << "Index of " << target << ": " << sol.search(nums, target) << endl;
+    return 0;
+}
