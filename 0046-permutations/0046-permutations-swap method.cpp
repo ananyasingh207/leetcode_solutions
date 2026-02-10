9e6641a45ff9a0c62acfc05d145ef74807fde6b6
Ananya Singh <150187328+ananyasingh207@users.noreply.github.com>
2026-02-10 22:38:46 +0530

    Time: 0 ms (100%), Space: 10.6 MB (70.59%) - LeetHub

---
+++ 0046-permutations/0046-permutations-swap method.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+
+    void swap(int& a, int& b){
+        int temp = a;
+        a = b;
+        b = temp;
+    }
+
+    void helper(vector<int>& nums, vector<vector<int>>& ans, int index){
+        if(index==nums.size()){
+            ans.push_back(nums);
+            return;
+        }
+        for(int i=index;i<nums.size();i++){
+            swap(nums[index],nums[i]);
+            helper(nums,ans,index+1);
+            swap(nums[index],nums[i]);
+        }
+    }
+
+    vector<vector<int>> permute(vector<int>& nums) {
+        vector<vector<int>> ans;
+        helper(nums,ans,0);
+        return ans;
+    }
+};
