93abc1273684ea174d7502c22ddb838916a26ef7
Ananya Singh <150187328+ananyasingh207@users.noreply.github.com>
2026-02-18 01:06:41 +0530

    Time: 2 ms (79.01%), Space: 27.4 MB (65.73%) - LeetHub

--- 0105-construct-binary-tree-from-preorder-and-inorder-traversal/0105-construct-binary-tree-from-preorder-and-inorder-traversal-hashmap.cpp
+++ 0105-construct-binary-tree-from-preorder-and-inorder-traversal/0105-construct-binary-tree-from-preorder-and-inorder-traversal-hashmap.cpp
@@ -12,21 +12,14 @@
 class Solution {
 public:
 
-    int search(vector<int>& inorder, int root, int left, int right){
-        for(int i=left;i<=right;i++){
-            if(inorder[i]==root){
-                return i;
-            }
-        }
-        return -1;
-    }
+    unordered_map<int,int> map;
 
     TreeNode* helper(vector<int>& preorder, vector<int>& inorder, int& idx, int left, int right){
         if(left>right){
             return nullptr;
         }
         TreeNode* root = new TreeNode(preorder[idx]);
-        int inIdx = search(inorder,preorder[idx],left,right);
+        int inIdx = map[preorder[idx]];
         idx++;
         root->left = helper(preorder,inorder,idx,left,inIdx-1);
         root->right = helper(preorder,inorder,idx,inIdx+1,right);
@@ -34,6 +27,9 @@ public:
     }
 
     TreeNode* buildTree(vector<int>& preorder, vector<int>& inorder) {
+        for(int i=0;i<inorder.size();i++){
+            map[inorder[i]] = i;
+        }
         int idx = 0;
         return helper(preorder,inorder,idx,0,inorder.size()-1);
     }
