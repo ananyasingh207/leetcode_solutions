6d86aee36542d58ce45267217d3cf93b1fc484a9
Ananya Singh <150187328+ananyasingh207@users.noreply.github.com>
2026-04-12 20:38:49 +0530

    Time: 50 ms (43.56%), Space: 51.1 MB (12.5%) - LeetHub

---
+++ 0560-subarray-sum-equals-k/0560-subarray-sum-equals-k.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    int subarraySum(vector<int>& nums, int k) {
+        unordered_map<int, int> freq;
+        int prefix = 0;
+        int count = 0;
+        freq[0] = 1;
+        for(int i=0;i<nums.size();i++){
+            prefix += nums[i];
+            int remove = prefix-k;
+            count+=freq[remove];
+            freq[prefix]+=1;
+        }
+        return count;
+    }
+};
