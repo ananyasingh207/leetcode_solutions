e556e149a8a4d55b7990e2d8f74b95563d8dfecd
Ananya Singh <150187328+ananyasingh207@users.noreply.github.com>
2026-04-18 01:19:38 +0530

    Time: 0 ms (100%), Space: 10.1 MB (88.14%) - LeetHub

--- 0198-house-robber/0198-house-robber.cpp
+++ 0198-house-robber/0198-house-robber.cpp
@@ -3,7 +3,7 @@ public:
     int rob(vector<int>& nums) {
         int prev1 = 0;
         int prev2 = 0;
-
+    
         for(int i=0;i<nums.size();i++){
             int take = nums[i]+prev2;
             int skip = prev1;
