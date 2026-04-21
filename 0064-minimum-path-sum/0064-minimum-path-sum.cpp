02ca5d215dc7bed864689e3567dfa10668e35a62
Ananya Singh <150187328+ananyasingh207@users.noreply.github.com>
2026-04-22 00:24:48 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0064-minimum-path-sum/0064-minimum-path-sum.cpp
+++ 0064-minimum-path-sum/0064-minimum-path-sum.cpp
@@ -2,7 +2,7 @@ class Solution {
 public:
     int getSum(vector<vector<int>>& grid, int n, int m, int r, int c, vector<vector<int>>& dp){
         if(r>n-1 || c>m-1){
-            return 100000;
+            return INT_MAX;
         }
         if(r==n-1 && c==m-1){
             dp[r][c]=grid[r][c];
@@ -11,9 +11,14 @@ public:
         if(dp[r][c]!=-1){
             return dp[r][c];
         }
-        int right = grid[r][c]+getSum(grid,n,m,r,c+1,dp);
-        int down = grid[r][c]+getSum(grid,n,m,r+1,c,dp);
-        dp[r][c]=min(right,down);
+        int right = getSum(grid,n,m,r,c+1,dp);
+        int down = getSum(grid,n,m,r+1,c,dp);
+        int best = min(right,down);
+        if(best == INT_MAX){
+            dp[r][c]=INT_MAX;
+            return dp[r][c];
+        }
+        dp[r][c] = best+grid[r][c];
         return dp[r][c];
     }
 
