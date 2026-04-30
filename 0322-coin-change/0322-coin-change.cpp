a3745a66d65f19e86c5c64fb85c1eee45a916cd6
Ananya Singh <150187328+ananyasingh207@users.noreply.github.com>
2026-04-30 23:42:45 +0530

    Time: 62 ms (11.38%), Space: 49.9 MB (18.72%) - LeetHub

--- 0322-coin-change/0322-coin-change.cpp
+++ 0322-coin-change/0322-coin-change.cpp
@@ -1,7 +1,7 @@
 class Solution {
 public:
     int inf = 1e9;
-    int minCoins(vector<int>& coins, int n, int amount, int index,vector<int>& dp){
+    int minCoins(vector<int>& coins, int n, int amount, int index,vector<vector<int>>& dp){
         if(index>=n){
             return inf;
         }
@@ -11,18 +11,18 @@ public:
         if(amount==0){
             return 0;
         }
-        if(dp[amount]!=-1){
-            return dp[amount];
+        if(dp[index][amount]!=-1){
+            return dp[index][amount];
         }
         int pick = 1+minCoins(coins,n,amount-coins[index],index,dp);
         int nonpick = minCoins(coins,n,amount,index+1,dp);
-        dp[amount] = min(pick,nonpick);
-        return dp[amount];
+        dp[index][amount] = min(pick,nonpick);
+        return dp[index][amount];
     }
 
     int coinChange(vector<int>& coins, int amount) {
         int n = coins.size();
-        vector<int> dp(amount+1,-1);
+        vector<vector<int>> dp(n,vector<int>(amount+1,-1));
         int ans = minCoins(coins,n,amount,0,dp);
         return (ans==inf ? -1 : ans);
     }
