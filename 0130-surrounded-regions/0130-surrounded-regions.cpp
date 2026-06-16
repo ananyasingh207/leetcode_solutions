9a819fd9e20fe24e4170a9489dd1a6bda69a2522
Ananya Singh <150187328+ananyasingh207@users.noreply.github.com>
2026-06-17 00:34:32 +0530

    Time: 0 ms (100%), Space: 14.8 MB (35.11%) - LeetHub

--- 0130-surrounded-regions/0130-surrounded-regions.cpp
+++ 0130-surrounded-regions/0130-surrounded-regions.cpp
@@ -3,6 +3,9 @@ public:
     void solve(vector<vector<char>>& board) {
         int n = board.size();
         int m = board[0].size();
+        if(n==0 || m==0){
+            return;
+        }
         vector<vector<bool>> isSafe(n, vector<bool>(m,false));
         queue<pair<int,int>> q;
         for(int i=0;i<n;i++){
