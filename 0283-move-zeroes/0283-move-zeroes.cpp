56db4485d354a03c8a05b75f97b36e4daf913f9f
Ananya Singh <150187328+ananyasingh207@users.noreply.github.com>
2026-06-23 00:19:04 +0530

    Time: 0 ms (100%), Space: 24 MB (18.82%) - LeetHub

--- 0283-move-zeroes/0283-move-zeroes.cpp
+++ 0283-move-zeroes/0283-move-zeroes.cpp
@@ -1,19 +1,23 @@
 class Solution {
 public:
+    void swap(int& a, int& b){
+        int temp = a;
+        a = b;
+        b = temp;
+    }
+
     void moveZeroes(vector<int>& nums) {
         int n = nums.size();
         int i = 0;
         int j = 0;
-        while(i<n){
+        while(i<n && j<n){
             if(nums[i]!=0){
-                nums[j]=nums[i];
+                if(i!=j){
+                    swap(nums[i],nums[j]);
+                }
                 j++;
             }
             i++;
-        }
-        while(j<n){
-            nums[j]=0;
-            j++;
         }
     }
 };
