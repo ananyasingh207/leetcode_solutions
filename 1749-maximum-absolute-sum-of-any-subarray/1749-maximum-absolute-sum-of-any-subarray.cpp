f65fba02c8d58eefd1137c003a854a5332fbbb3f
Ananya Singh <150187328+ananyasingh207@users.noreply.github.com>
2026-06-22 12:50:43 +0530

    Time: 0 ms (100%), Space: 45 MB (88.21%) - LeetHub

---
+++ 1749-maximum-absolute-sum-of-any-subarray/1749-maximum-absolute-sum-of-any-subarray.cpp
@@ -0,0 +1,39 @@
+class Solution {
+public:
+    int maxAbsoluteSum(vector<int>& nums) {
+        // int n = nums.size();
+        // int maxSum = -1;
+        // int sum = 0;
+        // int i = 0;
+        // while(i<n){
+        //     sum = abs(sum+nums[i]);
+        //     if(sum>maxSum){
+        //         maxSum = sum;
+        //     }
+        //     i++;
+        // }
+        // i = 0;
+        // sum = 0;
+        // while(i<n){
+        //     sum = abs(sum+nums[i]);
+        //     if(sum>maxSum){
+        //         maxSum = sum;
+        //     }
+        //     i++;
+        // }
+        // return maxSum;
+        int n = nums.size();
+        int localMax = nums[0];
+        int localMin = nums[0];
+        int globalMax = nums[0];
+        int globalMin = nums[0];
+        for(int i=1;i<n;i++){
+            localMax = max(nums[i], nums[i]+localMax);
+            localMin = min(nums[i], nums[i]+localMin);
+
+            globalMax = max(globalMax, localMax);
+            globalMin = min(globalMin, localMin);
+        }
+        return max(globalMax, abs(globalMin));
+    }
+};
