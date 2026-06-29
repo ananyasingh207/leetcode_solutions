20f0da645bf0fb3d0621494d96b598b717cba352
Ananya Singh <150187328+ananyasingh207@users.noreply.github.com>
2026-06-29 14:57:54 +0530

    Time: 740 ms (14.2%), Space: 156.4 MB (5.06%) - LeetHub

---
+++ 0127-word-ladder/0127-word-ladder.cpp
@@ -0,0 +1,37 @@
+class Solution {
+public:
+    int ladderLength(string beginWord, string endWord, vector<string>& wordList) {
+        unordered_map<string,bool> mp;
+        for(int i=0;i<wordList.size();i++){
+            mp[wordList[i]]=true;
+        }
+        if(mp.find(endWord)==mp.end()){
+            return 0;
+        }
+        unordered_map<string,bool> visited;
+        queue<pair<string,int>> q;
+        q.push({beginWord,1});
+        visited[beginWord] = true;
+        while(!q.empty()){
+            auto front = q.front();
+            string start = front.first;
+            int steps = front.second;
+            q.pop();
+            for(int i=0;i<start.size();i++){
+                char og = start[i];
+                for(char ch='a';ch<='z';ch++){
+                    start[i]=ch;
+                    if(start==endWord) return steps+1;
+                    if(!visited[start]){
+                        visited[start]=true;
+                        if(mp.find(start)!=mp.end()){
+                            q.push({start,steps+1});
+                        }
+                    }
+                }
+                start[i] = og;
+            }
+        }
+        return 0;
+    }
+};
