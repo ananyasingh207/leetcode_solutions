6c116d99e046a4b7352b422ba0676d203e04b750
Ananya Singh <150187328+ananyasingh207@users.noreply.github.com>
2026-07-08 11:24:53 +0530

    Time: 3 ms (68.5%), Space: 10.8 MB (93.31%) - LeetHub

--- 0424-longest-repeating-character-replacement/0424-longest-repeating-character-replacement.cpp
+++ 0424-longest-repeating-character-replacement/0424-longest-repeating-character-replacement.cpp
@@ -2,28 +2,25 @@ class Solution {
 public:
     int characterReplacement(string s, int k) {
         int n = s.length();
-        int maxLen = 0;
-        int i = 0;
-        int j = 0;
         vector<int> freq(26,0);
+        int maxLen = 0;
         int maxFreq = 0;
-        while(i<n && j<n){
-            freq[s[j]-'A']++;
-            maxFreq = max(maxFreq,freq[s[j]-'A']);
-            int windowSize = j-i+1;
-            int toReplace = windowSize - maxFreq;
-            while(toReplace>k){
-                freq[s[i]-'A']--;
-                int subFreq = 0;
+        int j = 0;
+        for(int i=0;i<n;i++){
+            freq[s[i]-'A']++;
+            maxFreq = max(maxFreq,freq[s[i]-'A']);
+            int windowSize = i-j+1;
+            int canReplace = windowSize - maxFreq;
+            while(canReplace>k){
+                freq[s[j]-'A']--;
+                j++;
+                int windowMax = 0;
                 for(int k=0;k<26;k++){
-                    subFreq = max(subFreq,freq[k]);
+                    windowMax = max(windowMax,freq[k]);
                 }
-                i++;
-                windowSize = j-i+1;
-                toReplace = windowSize - subFreq;
+                canReplace = i-j+1 - windowMax;
             }
-            maxLen = max(windowSize,maxLen);
-            j++;
+            maxLen = max(maxLen,i-j+1);
         }
         return maxLen;
     }
