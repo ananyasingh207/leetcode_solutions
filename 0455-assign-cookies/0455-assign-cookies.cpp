8d3106f6964b7472317b350b9e1bf1aaf9fc48e1
Ananya Singh <150187328+ananyasingh207@users.noreply.github.com>
2026-07-10 10:35:12 +0530

    Time: 19 ms (5.09%), Space: 44.8 MB (78.3%) - LeetHub

--- 0455-assign-cookies/0455-assign-cookies.cpp
+++ 0455-assign-cookies/0455-assign-cookies.cpp
@@ -3,16 +3,18 @@ public:
     int findContentChildren(vector<int>& g, vector<int>& s) {
         int n = g.size();
         int m = s.size();
-        sort(g.begin(),g.end());
         sort(s.begin(),s.end());
+        sort(g.begin(),g.end());
+        int count = 0;
         int i = 0;
         int j = 0;
         while(i<n && j<m){
-            if(s[j]>=g[i]){
+            if(g[i]<=s[j]){
+                count++;
                 i++;
             }
             j++;
         }
-        return i;
+        return count;
     }
 };
