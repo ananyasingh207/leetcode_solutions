d81e75383e6d029b3c16cb4ab1440221a1f93006
Ananya Singh <150187328+ananyasingh207@users.noreply.github.com>
2026-07-12 17:34:23 +0530

    Time: 55 ms (43.92%), Space: 94 MB (39.47%) - LeetHub

--- 0435-non-overlapping-intervals/0435-non-overlapping-intervals.cpp
+++ 0435-non-overlapping-intervals/0435-non-overlapping-intervals.cpp
@@ -6,16 +6,15 @@ public:
             if(a[0]==b[0]) return a[1]<b[1];
             else return a[0]<b[0];
         });
-        vector<vector<int>> lastInterval;
-        lastInterval.push_back(intervals[0]);
+        int lastInterval = intervals[0][1];
         int overlap = 0;
         for(int i=1;i<n;i++){
-            if(intervals[i][0]<lastInterval.back()[1] && intervals[i][1]>lastInterval.back()[0]){
+            if(intervals[i][0]<lastInterval){
                 overlap++;
-                lastInterval.back()[1] = min(lastInterval.back()[1],intervals[i][1]);
+                lastInterval = min(lastInterval,intervals[i][1]);
             }
             else{
-                lastInterval.back()[1] = intervals[i][1];
+                lastInterval = intervals[i][1];
             }
         }
         return overlap;
