339adb5ad4afb90ac24613ea4c702f0484555aef
Ananya Singh <150187328+ananyasingh207@users.noreply.github.com>
2026-07-15 23:59:35 +0530

    Time: 0 ms (100%), Space: 18.3 MB (93.01%) - LeetHub

--- 0023-merge-k-sorted-lists/0023-merge-k-sorted-lists.cpp
+++ 0023-merge-k-sorted-lists/0023-merge-k-sorted-lists.cpp
@@ -8,34 +8,34 @@
  *     ListNode(int x, ListNode *next) : val(x), next(next) {}
  * };
  */
+
+struct Compare{
+    bool operator()(ListNode* a, ListNode* b){
+        return a->val > b->val;
+    }
+};
+
 class Solution {
 public:
-    ListNode* merge(ListNode* list1, ListNode* list2){
-        if(!list1) return list2;
-        if(!list2) return list1;
-        ListNode* dummy = new ListNode(0);
-        ListNode* curr = dummy;
-        while(list1 && list2){
-            if(list1->val <= list2->val){
-                curr->next = list1;
-                list1=list1->next;
-            }
-            else{
-                curr->next = list2;
-                list2=list2->next;
-            }
-            curr=curr->next;
-        }
-        if(list1) curr->next = list1;
-        if(list2) curr->next = list2;
-        return dummy->next;
-    }
-
     ListNode* mergeKLists(vector<ListNode*>& lists) {
-        ListNode* res = nullptr;
+        priority_queue<ListNode*, vector<ListNode*>, Compare> pq;
         for(int i=0;i<lists.size();i++){
-            res = merge(res,lists[i]);
+            if(lists[i]){
+                pq.push(lists[i]);
+            }
         }
-        return res;
+        ListNode* dummy = new ListNode(-1);
+        ListNode* temp = dummy;
+        while(!pq.empty()){
+            ListNode* top = pq.top();
+            pq.pop();
+            temp->next = top;
+            if(top->next){
+                top=top->next;
+                pq.push(top);
+            }
+            temp=temp->next;
+        }
+        return dummy->next;
     }
 };
