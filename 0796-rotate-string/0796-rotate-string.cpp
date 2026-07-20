63e11c43212d9e8e17a8dbe739b04cf59d2b90f0
Ananya Singh <150187328+ananyasingh207@users.noreply.github.com>
2026-07-20 12:50:26 +0530

    Time: 0 ms (100%), Space: 8.2 MB (45.78%) - LeetHub

--- 0796-rotate-string/0796-rotate-string.cpp
+++ 0796-rotate-string/0796-rotate-string.cpp
@@ -7,7 +7,7 @@ public:
         if(s==goal) return true;
         for(int i=0;i<n;i++){
             //s.subtr(i) gives the entire string starting from i
-            //s.substr(0,i) gives a string startin from 0 having length i
+            //s.substr(0,i) gives a string starting from 0 having length i
             string rotated = s.substr(i) + s.substr(0,i);
             if(rotated==goal) return true;
         }
