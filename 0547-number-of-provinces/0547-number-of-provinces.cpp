4494089760d9565bc9a5d79257a6f4ef13a100bc
Ananya Singh <150187328+ananyasingh207@users.noreply.github.com>
2026-08-08 20:26:14 +0530

    Time: 0 ms (100%), Space: 19.4 MB (82.84%) - LeetHub

--- 0547-number-of-provinces/0547-number-of-provinces.cpp
+++ 0547-number-of-provinces/0547-number-of-provinces.cpp
@@ -1,27 +1,49 @@
 class Solution {
 public:
-    void dfs(vector<bool>& visited, int n, vector<vector<int>>& isConnected, int node){
-        if(visited[node]){
-            return;
+    vector<int> parent;
+    vector<int> rank;
+    int find(int num){
+        if(parent[num]==num){
+            return num;
         }
-        visited[node] = true;
-        for(int i=0;i<n;i++){
-            if(!visited[i] && isConnected[node][i]==1){
-                dfs(visited,n,isConnected,i);
-            }
+        return parent[num] = find(parent[num]);
+    }
+
+    void unionSet(int& a, int& b){
+        int parA = find(a);
+        int parB = find(b);
+        if(parA==parB) return;
+        if(rank[parA]==rank[parB]){
+            parent[parB] = parA;
+            rank[parA]++;
+        }
+        else if(rank[parA]>rank[parB]){
+            parent[parB] = parA;
+        }
+        else{
+            parent[parA] = parB;
         }
     }
 
     int findCircleNum(vector<vector<int>>& isConnected) {
         int n = isConnected.size();
-        vector<bool> visited(n,false);
-        int count = 0;
+        rank.resize(n,0);
+        parent.resize(n);
         for(int i=0;i<n;i++){
-            if(!visited[i]){
-                count++;
-                dfs(visited,n,isConnected,i);
+            parent[i]=i;
+        }
+        int provinces = n;
+        for(int i=0;i<n;i++){
+            for(int j=0;j<n;j++){
+                if(isConnected[i][j]==1){
+                    if(find(i)==find(j)) continue;
+                    else{
+                        unionSet(i,j);
+                        provinces--;
+                    }
+                }
             }
         }
-        return count;
+        return provinces;
     }
 };
