c24295a1d2b2ff9811f24294be92c477c2ff97cb
Ananya Singh <150187328+ananyasingh207@users.noreply.github.com>
2026-08-16 22:16:21 +0530

    Time: 57 ms (59.96%), Space: 122.5 MB (88.94%) - LeetHub

---
+++ 1483-kth-ancestor-of-a-tree-node/1483-kth-ancestor-of-a-tree-node.cpp
@@ -0,0 +1,43 @@
+class TreeAncestor {
+public:
+    int n;
+    int LOG;
+    vector<vector<int>> up;
+
+    TreeAncestor(int n, vector<int>& parent) {
+        this->n = n;
+
+        this->LOG = 16;
+        up.resize(n, vector<int>(LOG, -1));
+
+        for(int i=0;i<n;i++){
+            up[i][0]=parent[i];
+        }
+
+        for(int j=1;j<LOG;j++){
+            for(int i=0;i<n;i++){
+                if(up[i][j-1]!=-1){
+                    up[i][j] = up[ up[i][j-1] ][j-1];
+                }
+            }
+        }
+    }
+    
+    int getKthAncestor(int node, int k) {
+        for(int j=0;j<LOG;j++){
+            if(k&(1<<j)){
+                node = up[node][j];
+            }
+            if(node==-1) return -1;
+        }
+        return node;
+    }
+};
+
+/**
+ * Your TreeAncestor object will be instantiated and called as such:
+ * TreeAncestor* obj = new TreeAncestor(n, parent);
+ * int param_1 = obj->getKthAncestor(node,k);
+ */
+
+ 
