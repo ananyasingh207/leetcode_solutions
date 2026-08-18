1920ac9895bf687976d157fac245b28b605e1ce8
Ananya Singh <150187328+ananyasingh207@users.noreply.github.com>
2026-08-18 21:08:31 +0530

    Time: 0 ms (100%), Space: 24.3 MB (66.78%) - LeetHub

--- 0230-kth-smallest-element-in-a-bst/0230-kth-smallest-element-in-a-bst.cpp
+++ 0230-kth-smallest-element-in-a-bst/0230-kth-smallest-element-in-a-bst.cpp
@@ -12,7 +12,7 @@
 class Solution {
 public:
     void traverse(TreeNode* root, int& index,int& ans,int k){
-        if(root==nullptr){
+        if(root==nullptr || index>=k){
             return;
         }
         traverse(root->left,index,ans,k);
