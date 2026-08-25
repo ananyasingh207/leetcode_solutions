d4c0ae03fbb747f0d4c794e40bfc1490bc048978
Ananya Singh <150187328+ananyasingh207@users.noreply.github.com>
2026-08-26 01:23:47 +0530

    Time: 2 ms (30.45%), Space: 17.4 MB (10.57%) - LeetHub

--- 0994-rotting-oranges/0994-rotting-oranges.cpp
+++ 0994-rotting-oranges/0994-rotting-oranges.cpp
@@ -3,36 +3,36 @@ public:
     int orangesRotting(vector<vector<int>>& grid) {
         int n = grid.size();
         int m = grid[0].size();
-        queue<pair<pair<int, int>, int>> q;
-        vector<vector<bool>> visited(n, vector<bool>(m, false));
+        vector<vector<bool>> visited(n,vector<bool>(m,false));
+        queue<pair<pair<int,int>,int>> q;
         int fresh = 0;
-        for (int i = 0; i < n; i++) {
-            for (int j = 0; j < m; j++) {
-                if (grid[i][j] == 2) {
-                    q.push({{i, j}, 0});
-                    visited[i][j] = true;
+        for(int i=0;i<n;i++){
+            for(int j=0;j<m;j++){
+                if(grid[i][j]==2){
+                    q.push({{i,j},0});
+                    visited[i][j]=true;
                 }
-                if(grid[i][j]==1){
+                else if(grid[i][j]==1){
                     fresh++;
                 }
             }
         }
         int ans = 0;
-        vector<int> dr = {1,0,0,-1};
-        vector<int> dc = {0,1,-1,0};
-        while (!q.empty()) {
-            auto temp = q.front();
+        vector<int> dx = {0,1,-1,0};
+        vector<int> dy = {1,0,0,-1};
+        while(!q.empty()){
+            auto front = q.front();
+            int r = front.first.first;
+            int c = front.first.second;
+            int steps = front.second;
+            ans = max(ans,steps);
             q.pop();
-            int i = temp.first.first;
-            int j = temp.first.second;
-            int t = temp.second;
-            ans = max(ans, t);
-            for(int k=0;k<4;k++){
-                int r = i+dr[k];
-                int c = j+dc[k];
-                if(r>=0 && c>=0 && r<n && c<m && !visited[r][c] && grid[r][c]==1){
-                    q.push({{r,c},t+1});
-                    visited[r][c] = true;
+            for(int i=0;i<4;i++){
+                int row = r+dx[i];
+                int col = c+dy[i];
+                if(row>=0 && row<n && col>=0 && col<m && !visited[row][col] && grid[row][col]==1){
+                    q.push({{row,col},steps+1});
+                    visited[row][col]=true;
                     fresh--;
                 }
             }
